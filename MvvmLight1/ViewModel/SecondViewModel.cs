cad72997f0ab573093d0eae23bbb5003ec005878
Andrii Kovalchuk <tieorange@gmail.com>
2015-10-18 18:57:41 +0200

    init

---
+++ MvvmLight1/ViewModel/SecondViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace MvvmLight1.ViewModel
+{
+    public class SecondViewModel : ViewModelBase
+    {
+
+        private int _counter = 777;
+        private RelayCommand _navigateCommand;
+        private RelayCommand _showMessageBox;
+        private RelayCommand _decrementCommand;
+
+
+        public SecondViewModel(IMessenger messenger) : base(messenger)
+        {
+        }
+
+        public RelayCommand NavigateComamnd => _navigateCommand
+                                               ??
+                                               (_navigateCommand = new RelayCommand(
+                                                   () =>
+                                                   {
+
+                                                   }
+                                                   ));
+    }
+}
